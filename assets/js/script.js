6aba86ba24ea8986aec7fa41de0536ca4349a92c
sinhast <dylanv.dev@gmail.com>
2019-09-29 21:09:38 +0200

    Add a function in script.js to switch the class 'active'

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ const previousSlide = document.querySelector(".previousSlide");
 const images = document.querySelectorAll("li");
 const button = document.querySelector("button");
 let i = 0;
+
+function scrollingNext() {
+  images[i].classList.remove("active");
+  i++;
+  if (i >= document.images.length) {
+    i = 0;
+  }
+  images[i].classList.add("active");
+}
