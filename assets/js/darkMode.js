a30f63a74db653caa36ae38f926fb97a17ba83cd
sinhast <dylanv.dev@gmail.com>
2019-09-29 21:22:41 +0200

    Add the function for the dark mode in darkMode.js

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,1 +1,13 @@
 const darkMode = document.querySelector("#darkMode");
+
+function changeTheme(ev) {
+  if (ev.target.tagName === "A") {
+    document.body.classList.toggle("darkMode");
+  }
+
+  if (document.querySelector("body").classList.contains("darkMode")) {
+    document.querySelector("#darkMode").classList.add("active");
+  } else {
+    document.querySelector("#darkMode").classList.remove("active");
+  }
+}
