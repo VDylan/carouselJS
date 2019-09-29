de538e0a2b1a406f8ff33cb7f1a1880cc3ea586a
sinhast <dylanv.dev@gmail.com>
2019-09-29 21:00:05 +0200

    Add all needed variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const nextSlide = document.querySelector(".nextSlide");
+const previousSlide = document.querySelector(".previousSlide");
+const images = document.querySelectorAll("li");
+const button = document.querySelector("button");
+let i = 0;
